66cc30a6db68dfe9f4284673c7c1dadbafb04b44
robbie <robbiekruszynski@gmail.com>
2021-05-11 11:34:14 -0700

    render render render

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,43 +1,59 @@
 import React, { Component } from 'react';
 import './Chart.css';
+import Slices from './Slices/Slices';
 
 export default class Chart extends Component {
     state = {
-        1: {
-            spoke: 'Truffle',
-            rotate: 'rotate(0.0000, 220, 220)',
-            fill: '#FFSC00',
-            transform: '0.1'
-
-        },
-        2: {
-            spoke: 'Infura',
-            rotate: 'rotate(45.0000, 220, 220)',
-            fill: '#FFSC05',
-            transform: '0.1'
-
-        },
-        3: {
-            spoke: 'ConsenSys Quorum',
-            rotate: 'rotate(90.0000, 220, 220)',
-            fill: '#FFSC20',
-            transform: '0.1'
-
-        },
-        4: {
-            spoke: 'MetaMask',
-            rotate: 'rotate(135.0000, 220, 220)',
-            fill: '#FFSC50',
-            transform: '0.1'
-
-        },
-        5: {
-            spoke: 'Diligence',
-            rotate: 'rotate(180.0000, 220, 220)',
-            fill: '#FFSC50',
-            transform: '0.1'
-
-        },
+        slices: {
+            1: {
+                question: 'Truffle',
+                rotate: 'rotate(0.0000, 220, 220)',
+                fill: '#FF5C00',
+                transform: '0.1'
+            },
+            2: {
+                question: 'Infura',
+                rotate: 'rotate(45.0000, 220, 220)',
+                fill: '#9245FF',
+                transform: '0.1'
+            },
+            3: {
+                question: 'Diligence',
+                rotate: 'rotate(90.0000, 220, 220)',
+                fill: '#FF234B',
+                transform: '0.1'
+            },
+            4: {
+                question: 'ConsenSys Quorum',
+                rotate: 'rotate(135.0000, 220, 220)',
+                fill: '#357AFF',
+                transform: '0.1'
+            },
+            5: {
+                question: 'ConsenSys Academy',
+                rotate: 'rotate(180.0000, 220, 220)',
+                fill: '#E5AC00',
+                transform: '0.1'
+            },
+            6: {
+                question: 'MetaMask',
+                rotate: 'rotate(225.0000, 220, 220)',
+                fill: '#00AB3A',
+                transform: '0.1'
+            },
+            7: {
+                question: 'AMB',
+                rotate: 'rotate(270.0000, 220, 220)',
+                fill: '#00A9B5',
+                transform: '0.1'
+            },
+            8: {
+                question: 'BUIDL',
+                rotate: 'rotate(315.0000, 220, 220)',
+                fill: '#E5AC00',
+                transform: '0.1'
+            },
+        }
     }
     render(){
         return(
@@ -47,25 +63,24 @@ export default class Chart extends Component {
                         <li>Content</li>
                     </ul>
               </div>
-            <div className="ChartBlock">
-                <div className="ChartShow" style={{width: '450px'}}>
-                    <svg className="ChartBody" width="440px" height="440px" viewBox="0 0 440 400" xmlns="http://www.w3.org/svg">
+              <div className="ChartBlock">
+                    <div className="ChartPie" style={{width: '450px'}}>
+                        <svg className="ChartPieChunk" width="440px" height="440px" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
                         <defs>
                             <g id="overlay">
                                 <rect x="219" y="20" height="200" width="2" fill="#FFF"></rect>
+                                <g transform="translate(210, 10)">
+                                    <path fill="#000"></path>
+                                </g>
                             </g>
                         </defs>
                         <mask id="wedge-mask" fill="white">
-                            <path trasnform="trasnalte(20, 20)" d="M93 7.241a200.006 200.006 0 01173.551-.865L200.004 200 112.33 20.241z" fillRule="nonezero"></path>
+                            <path transform="translate(20, 20)" d="M93 7.241a200.006 200.006 0 01173.551-.865L200.004 200 112.33 20.241z" fillRule="nonzero"></path>
                         </mask>
-                        <circle cx="220" cy="220" fill="##EFEFEF" strok="E3E3E3" strokeWidth="1"></circle>
-                        <g transform="roatate (0.0000, 220, 220">
-                            <g mask="url(#wedge-mask)">
-                                <circle className="Slice" cx="220" cy="220" r="200" fill="#FF5C00" style={{transform: 'scale(0.5)'}}></circle>
-                            </g>
-
-                        </g>
-
+                        <circle cx="220" cy="220" r="200" fill="#EFEFEF" stroke="#E3E3E3" strokeWidth="1"></circle>
+                            <Slices
+                                slices={this.state.slices}
+                            />
                     </svg>
                 </div>
              </div>
